caf800cb2daeb0b753bfb9266bee444b5e535a17
wojtekqqq <wojtekqq@tlen.pl>
2020-02-29 15:14:04 +0100

    test

---
+++ cmake-build-debug/CMakeFiles/untitled5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled5.dir/main.cpp.o"
+  "libuntitled5.dll.a"
+  "untitled5.exe"
+  "untitled5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int ile_liczb;
+    int pierwsza;
+    int kolejne;
+    cout << "Ile liczb chcesz podac ? " << endl;
+    cin >>ile_liczb;
+    cout << "Podaj pierwsza liczbe" << endl;
+    cin >> pierwsza;
+    int max = pierwsza;
+    for (int i = 1; i < ile_liczb ; ++i) {
+        cout << "Podaj kolejna liczbe" << endl;
+        cin >> kolejne;
+        if (kolejne > max)
+        {
+            max = kolejne;
+        }
+
+    }
+    cout << max;
+    //testowy commmit
+
+    return 0;
+}
